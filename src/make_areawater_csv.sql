11e1d42dee1615feb8474bff34c6fcd700476165
bh <dbb@i7c.(none)>
2014-05-20 12:02:46 -0700

    path

--- src/make_areawater_csv.sql
+++ src/make_areawater_csv.sql
@@ -9,8 +9,9 @@ copy
     mtfcc as "MTFCC", 
     st_astext(geom) as "geom", 
     '-' as "text"
-FROM areawater) to '/home/shared/hsr_tx_areawater.csv'
- with CSV header delimiter E';';
+FROM areawater) 
+  to '/home/shared/geodata_misc/HSR_Texas_Geo_benchmark_data/hsr_tx_areawater.csv'
+   with CSV header delimiter E';';
 
 -- curl 'http://localhost:8983/solr/hsr_areawater/update/?commit=true&separator=;&escape=\&stream.file=/home/shared/geodata_misc/HSR_Texas_Geo_benchmark_data/hsr_tx_areawater_part0.csv&stream.contentType=text/csv;charset=utf-8&trim=false'
 --
